68d7b2a130c5dc83eb0a25d6537c85ebd15b2922
EdoardoMenicucci <edoardo.menicucci@libero.it>
2024-08-29 17:37:26 +0200

    deliveboo model migration seeder

---
+++ database/migrations/2024_08_29_141430_create_restaurants_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::disableForeignKeyConstraints();
+
+        Schema::create('restaurants', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('tax_id');
+            $table->string('address');
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->string('img');
+            $table->timestamps();
+        });
+
+        Schema::enableForeignKeyConstraints();
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('restaurants');
+    }
+};
---
+++ database/migrations/2024_08_29_141434_create_orders_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::disableForeignKeyConstraints();
+
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->timestamp('order_date');
+            $table->foreignId('restaurant_id')->constrained()->onDelete('cascade');
+            $table->decimal('total_price');
+            $table->string('buyer_name');
+            $table->string('buyer_address');
+            $table->string('buyer_phone');
+            $table->timestamps();
+        });
+
+        Schema::enableForeignKeyConstraints();
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('orders');
+    }
+};
